c200d3ae56664ed98ad60c0d9c4d84aba04aaa46
Pablo Arriagada <parriagada@Pablo-Arriagada-MacBook-Pro.local>
2020-06-06 20:39:31 -0400

    initial commit

---
+++ src/main/java/cl/parriagada/ms/person/MsPersonApplication.java
@@ -0,0 +1,13 @@
+package cl.parriagada.ms.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPersonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/parriagada/ms/person/MsPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.parriagada.ms.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
